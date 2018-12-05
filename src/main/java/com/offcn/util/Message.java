6b2b008dc62c8af4ffb919459ed2a5f3bddc0068
myj <eminemyj@163.com>
2018-12-05 18:59:56 +0800

    编写message工具类实体 添加set get方法

--- src/main/java/com/offcn/util/Message.java
+++ src/main/java/com/offcn/util/Message.java
@@ -1,5 +1,26 @@
 package com.offcn.util;
 
 public class Message {
-	 
+	/*
+	 * ´æ´¢·µ»ØÖµ
+	 */
+	private Object data;
+	private String view;
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+
+	public String getView() {
+		return view;
+	}
+
+	public void setView(String view) {
+		this.view = view;
+	}
+
 }
